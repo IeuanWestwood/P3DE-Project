9adbb0460683c1acebaaef3a29d0da655231a6a3
Ieuan Westwood <ieuan2.westwood@live.uwe.ac.uk>
2024-04-03 21:39:39 +0100

    Added Water drinking

--- Assets/Scripts/DrinkWater.cs
+++ Assets/Scripts/DrinkWater.cs
@@ -6,10 +6,12 @@ public class DrinkWater : MonoBehaviour
 {
 
     public Animator animator;
+    public GameObject ui;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        HideUI();
     }
 
     // Update is called once per frame
@@ -18,9 +20,44 @@ public class DrinkWater : MonoBehaviour
         
     }
 
-    private void OnMouseDown()
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ShowUI();
+
+        }
+    }
+
+    public void Drink()
     {
         animator.SetTrigger("Clicked");
     }
-    
+
+    public void HideUI()
+    {
+        ui.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+    public void ShowUI()
+    {
+        ui.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void PressYes()
+    {
+        HideUI();
+        Drink();
+    }
+
+    public void PressNo()
+    {
+        HideUI();
+    }
+
 }
